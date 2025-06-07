2f3eba9c75ecbd010d3fdf2251dbe0ca99d32ebf
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-06-07 16:34:04 -0500

    removing old makers and packergers, lets start fresh

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,6 +1,3 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -9,36 +6,8 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        iconUrl: 'https://getapptrack.com/icon.ico',
-        setupIcon: './img/icon.ico',
-      }
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      icon: './img/icon.icns',
-      format: 'ULFO',
-      platforms: ['darwin'],
-    }
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
     {
       name: '@electron-forge/plugin-webpack',
       config: {
@@ -54,18 +23,6 @@ module.exports = {
             }
           }]
         }
-      }
-    }
-  ],
-  publishers: [
-    {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'Caffeine-Driven-Development-LLC',
-          name: 'AppTrack'
-        },
-        prerelease: false
       }
     }
   ],
